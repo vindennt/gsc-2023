5f820c3b636a33172b6d069ac32f09295411964f
Nathan <nathanlu_@hotmail.com>
2023-03-21 18:30:18 -0700

    Finished landing page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,16 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+
+const theme = extendTheme({
+  fonts: {
+    heading: `'Merriweather', serif`,
+    body: `'Open Sans', sans-serif`,
+  },
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (<ChakraProvider>
+  return (<ChakraProvider theme={theme}>
     <Component {...pageProps} />
     </ChakraProvider>)
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,9 @@
 import { Heading } from '@chakra-ui/react'
+import Landing from '@/components/Landing';
 
 
 export default function Home() {
   return (
-
-      <Heading>
-        fuck
-      </Heading>
-      
+		<Landing />
   )
 }
