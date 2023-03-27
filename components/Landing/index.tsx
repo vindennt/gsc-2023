404991cbdc431b9d101a2d84eab2add52e4fcbb2
Jasper Huynh <107389291+Jahu5474@users.noreply.github.com>
2023-03-27 16:02:16 -0700

    fixed marker rendering. updated the map and scroll display.

--- components/Landing/index.tsx
+++ components/Landing/index.tsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import { Container, Stack, Flex, SimpleGrid, Heading, Text, Box, Button } from '@chakra-ui/react';
 import { FaRecycle, FaBatteryHalf } from 'react-icons/fa';
-import { FiArrowRight }from 'react-icons/fi';
+import { FiArrowRight } from 'react-icons/fi';
 import { AiOutlineShopping, AiOutlineSearch } from 'react-icons/ai';
+import Link from 'next/link';
 
 
 const NavLink = ({
 	title,
 	icon,
-} : {
+}: {
 	title: string,
 	icon: React.ReactNode
 }) => {
 	return (
 		<Stack direction="row" alignItems="center">
-			{icon}	
+			{icon}
 			<Text>
 				{title}
 			</Text>
@@ -23,7 +24,7 @@ const NavLink = ({
 }
 const Navbar = () => {
 	return (
-		<Stack	
+		<Stack
 			position="absolute"
 			w="100%"
 			backgroundColor="#FFF6D6"
@@ -57,9 +58,9 @@ const Card = ({
 	icon: React.ReactNode
 }) => {
 	return (
-		<Box 
+		<Box
 			h="300px"
-			borderRadius="10px" 
+			borderRadius="10px"
 			border="1px"
 			borderColor="gray.300"
 			boxShadow="lg"
@@ -67,31 +68,34 @@ const Card = ({
 			textAlign="left"
 			p={8}
 			_hover={{
-				boxShadow:"xl",
+				boxShadow: "xl",
 				marginTop: '-8px'
 			}}
 			cursor="pointer"
 			transition="all .2s"
 		>
-			<Stack gap={1}>
-				{icon}
-				<Text color={color} fontWeight="bold">
-					{subtitle}
-				</Text>
-				<Heading fontSize="2xl">
-					{title}
-				</Heading>
-				<Text>
-					{content}
-				</Text>
-
-				<Stack direction="row" alignItems="center" pt={2}>
-					<Text fontWeight="bold">
-						Get started
+			<Link href="/mapping">
+				<Stack gap={1}>
+					{icon}
+					<Text color={color} fontWeight="bold">
+						{subtitle}
 					</Text>
-					<FiArrowRight fontWeight="bold" />
+					<Heading fontSize="2xl">
+						{title}
+					</Heading>
+					<Text>
+						{content}
+					</Text>
+
+					<Stack direction="row" alignItems="center" pt={2}>
+
+						<Text fontWeight="bold">
+							Get started
+						</Text>
+						<FiArrowRight fontWeight="bold" />
+					</Stack>
 				</Stack>
-			</Stack>
+			</Link>
 		</Box>
 	)
 }
@@ -100,42 +104,42 @@ const Landing = () => {
 	return (
 		<>
 			<Navbar />
-		<Flex 
-			backgroundColor="#FFF6D6"
-			minH="100vh"
-			alignItems="center"
-			justifyContent="center"
-			pt={2}
-			backgroundImage="url(/background.png)"
-			backgroundRepeat="no-repeat"
-			backgroundPosition="bottom"
-		>
-			<Container maxW="container.xl" textAlign="center">
-				<Heading fontSize="5xl" fontWeight="bold" mb={3}>
-					It's what's on the inside that counts.
-				</Heading>
-				<Text>
-					Geo location app for electronic recycling, batteries, and can. Find and buy sustainable consumables.
-				</Text>
+			<Flex
+				backgroundColor="#FFF6D6"
+				minH="100vh"
+				alignItems="center"
+				justifyContent="center"
+				pt={2}
+				backgroundImage="url(/background.png)"
+				backgroundRepeat="no-repeat"
+				backgroundPosition="bottom"
+			>
+				<Container maxW="container.xl" textAlign="center">
+					<Heading fontSize="5xl" fontWeight="bold" mb={3}>
+						It's what's on the inside that counts.
+					</Heading>
+					<Text>
+						Geo location app for electronic recycling, batteries, and can. Find and buy sustainable consumables.
+					</Text>
 
-				<SimpleGrid mt={12} columns={2} spacing={10}>
-					<Card 
-						subtitle="Recycle"
-						title="What do you want to recycle"
-						content="Geo location app for electronic recycling, batteries, and can. Find and buy sustainable consumables."
-						icon={<FaBatteryHalf fontSize="48px" />}
-						color="#189D65" 
-					/>
-					<Card 
-						subtitle="Buy sustainable"
-						title="What do you want to recycle"
-						content="Geo location app for electronic recycling, batteries, and can. Find and buy sustainable consumables."
-						icon={<FaRecycle fontSize="48px" />} 
-						color="#F88215" 
-					/>
-				</SimpleGrid>
-			</Container>
-		</Flex>
+					<SimpleGrid mt={12} columns={2} spacing={10}>
+						<Card
+							subtitle="Recycle"
+							title="What do you want to recycle"
+							content="Geo location app for electronic recycling, batteries, and can. Find and buy sustainable consumables."
+							icon={<FaBatteryHalf fontSize="48px" />}
+							color="#189D65"
+						/>
+						<Card
+							subtitle="Buy sustainable"
+							title="What do you want to recycle"
+							content="Geo location app for electronic recycling, batteries, and can. Find and buy sustainable consumables."
+							icon={<FaRecycle fontSize="48px" />}
+							color="#F88215"
+						/>
+					</SimpleGrid>
+				</Container>
+			</Flex>
 		</>
 	)
 }
---
+++ pages/mapping.tsx
@@ -0,0 +1,35 @@
+
+import { Box, Container, Flex, Text, Stack, Center } from '@chakra-ui/react'
+import { VStack, StackDivider, HStack } from '@chakra-ui/react'
+import MapScroll from '../components/mapScroll'
+import Map from './Map'
+import Link from 'next/link'
+
+
+
+
+const Home = () => {
+
+
+  return (
+    <>
+      <Center>
+        <Box w="100%" h="100vh" overflow="hidden">
+          <Flex alignItems="center">
+            <Box overflowY="auto">
+              <MapScroll />
+            </Box>
+            <Box w="500px" flex="1" >
+              <Map />
+            </Box>
+
+          </Flex>
+
+        </Box>
+      </Center>
+    </>
+
+  )
+}
+
+export default Home;
