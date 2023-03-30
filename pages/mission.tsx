c6786b1e8c23c8f2474c25c0efda55439b1fbf6f
dennistruong <truong.dennis00@gmail.com>
2023-03-29 22:57:04 -0700

    mission statement

--- pages/mission.tsx
+++ pages/mission.tsx
@@ -1,12 +1,36 @@
-import React from 'react';
-import { Text } from '@chakra-ui/react';
+import React from "react";
+import { Text } from "@chakra-ui/react";
 
 const Mission = () => {
-    return (
-        <>
-            <Text>Mission</Text>
-        </>
-    )
-}
+  return (
+    <>
+      <Text>
+        Our mission is to raise awareness about the detrimental effects of
+        improper disposal of electronics and textiles on the environment, and to
+        promote sustainable solutions that mitigate pollution. Improper disposal
+        of electronics is a significant source of pollution, as e-waste contains
+        toxic substances such as lead, cadmium, and mercury that can leach into
+        the soil and waterways. According to the United Nations, the world
+        generates approximately 50 million tonnes of e-waste each year, and only
+        a fraction of this is properly recycled or disposed of. This leads to
+        environmental pollution, as well as health hazards for humans and
+        animals. In addition to electronics, textiles are also a major
+        contributor to waste and pollution. The fashion industry is known for
+        its fast-paced production cycles, which lead to a high volume of
+        discarded clothing and textile waste. According to the Ellen MacArthur
+        Foundation, the equivalent of one garbage truck full of textiles is
+        landfilled or burned every second globally. Textile production also
+        consumes large amounts of resources, including water, energy, and
+        chemicals, which contributes to environmental degradation. As a society,
+        we need to take action to reduce the impact of electronics and textiles
+        on the environment. This includes implementing sustainable practices for
+        the production and disposal of these materials, promoting the use of
+        recycled materials, and encouraging consumers to make environmentally
+        conscious choices. By working together, we can create a healthier and
+        more sustainable future for ourselves and future generations.
+      </Text>
+    </>
+  );
+};
 
 export default Mission;
