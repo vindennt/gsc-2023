d9add93560087a5e890bcb32e575e9c7d9c3b48d
dennistruong <truong.dennis00@gmail.com>
2023-04-04 16:29:57 -0700

    finished shop card

--- pages/shop.tsx
+++ pages/shop.tsx
@@ -1,10 +1,69 @@
 import React from 'react';
-import { Text } from '@chakra-ui/react';
+import { Text, Box, Link, VStack, Heading, Container } from '@chakra-ui/react';
+//import ShopCard from '@/components/shopCard';
+
+import data from '../data/shops.json';
+
+const parsedData = data.shop;
+
+
+const ShopCard = ({children, href, ...props}) => {
+    return (
+        <Link href={href} target="_blank">
+            <Box
+                h="300px"
+                w="100%"
+                borderRadius="10px"
+                border="1px"
+                borderColor="gray.300"
+                boxShadow="lg"
+                background="white"
+                textAlign="left"
+                p={8}
+                margin="5"
+                // _hover={{
+                //     boxShadow: "xl",
+                //     marginTop: '-8px'
+                // }}
+                cursor="pointer"
+                transition="all .2s"
+                
+                {...props}
+            >
+                {children}
+            </Box>
+        </Link>
+    )
+};
 
 const Shop = () => {
     return (
         <>
-            <Text>Shop</Text>
+        <Box
+        backgroundColor="#FFF9F2">
+            <Container
+                minH="100vh"
+                
+				alignItems="center"
+				justifyContent="center"
+				pt={2}
+				backgroundRepeat="no-repeat"
+				backgroundPosition="bottom"
+                >
+            
+                {parsedData.map((item) => (
+                // <Link href={item.link}>
+                    <ShopCard key={item.name} href={item.link} >
+                        <VStack gap={1} >
+                            <Text>{item.name}</Text>
+                            <Text fontSize="md">{item.description}</Text>
+                            <Text fontSize="md"> Products: {item['product-type']}</Text>
+                            <Link fontSize="md"> Link: {item.link}</Link>
+                        </VStack>
+                    </ShopCard>
+          ))}
+            </Container>
+        </Box>
         </>
     )
 }
